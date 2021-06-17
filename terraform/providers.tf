324101e26dd1ed623ab6ba164c01bdee25503729
Damien Gallagher <damien.gallagher@gmail.com>
2021-06-17 18:30:26 +0100

    Updated providers to reference to github cloud!

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,10 +6,20 @@ terraform {
     }
   }
   required_version = ">= 0.15.3"
+
+  backend "remote" {
+    organization = "SmallWindow21"
+
+    workspaces {
+      name = "SmallWindow-Workspace"
+    }
+  }
 }
 
 provider "aws" {
-  profile = var.aws_profile
+  #profile = var.aws_profile
   region  = var.region
   # remember that if you change the region you will also need to change the ami
 }
+
+
