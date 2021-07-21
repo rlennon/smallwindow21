1ffc70b343d2256500e555604052199a1dee108b
Damien Gallagher <damien.gallagher@gmail.com>
2021-07-21 17:19:20 +0100

    Tweaked for windows machine

--- app/scripts/Generate Javadoc.bat
+++ app/scripts/Generate Javadoc.bat
@@ -1,2 +1,3 @@
-../mvnw.cmd -f ../pom.xml clean javadoc:javadoc
-start chrome ../target/site/apidocs/index.html
+call ../mvnw.cmd -f ../pom.xml clean javadoc:javadoc
+call set currentPath=%cd%
+call start chrome %currentPath%/../target/site/apidocs/index.html
